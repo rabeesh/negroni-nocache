32835ef2b9d11a8c61bebc9eceb1cf0e2d97504d
rab <rabeeshkumar@gmail.com>
2015-07-12 15:25:08 +0400

    No cache middleware negroni

---
+++ nocache.go
@@ -0,0 +1,40 @@
+package nocache
+
+import (
+    "log"
+    "os"
+    "fmt"
+    "net/http"
+    "github.com/codegangsta/negroni"
+)
+
+type NoCache struct {
+    noEtag bool
+    Logger  *log.Logger
+}
+
+func NewNoCache(noEtag bool) *NoCache {
+    return &NoCache{
+        noEtag: noEtag,
+        Logger: log.New(os.Stdout, "[negroni no cache enabled] ", 0),
+    }
+}
+
+func (c *NoCache) ServeHTTP(rw http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
+    newRw := negroni.NewResponseWriter(rw)
+    newRw.Before(func (rw negroni.ResponseWriter){
+        c.setNoCacheHeader(rw)
+    })
+
+    next(newRw, req)
+}
+
+func (c *NoCache) setNoCacheHeader(rw http.ResponseWriter) {
+    h := rw.Header()
+    h.Set("Cache-Control", "private, no-store, no-cache, must-revalidate, proxy-revalidate")
+    h.Set("Pragma", "no-cache")
+    h.Set("Expires", "0")
+    if (c.noEtag) {
+      h.Del("ETag")
+    }
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+# negroni middleware for nocache
+
+Negroni middleware for no cache http headers
+
+## Usage
+
+
+~~~ go
+package main
+
+import (
+    "fmt"
+    "github.com/codegangsta/negroni"
+    "github.com/rabeesh/negroni-nocache"
+    "net/http"
+)
+
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+        fmt.Fprintf(rw, "Welcome to the home page!")
+    })
+
+    n := negroni.New()
+    n.Use(nocache.NewNoCache(true))
+    n.Use(negroni.NewStatic(http.Dir("public")))
+    n.UseHandler(mux)
+    n.Run(":5000")
+}
+~~~
